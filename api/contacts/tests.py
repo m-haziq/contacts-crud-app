238495c6968e15aad3fa386d8c32852a6f515f8e
M haziq <m_haziq@outlook.com>
2019-12-05 19:34:15 +0500

    added validation tests to django

--- api/contacts/tests.py
+++ api/contacts/tests.py
@@ -6,14 +6,43 @@ from .models import Contact
 class ContactTests(TestCase):
     client = APIClient()
 
+    # Validation Tests
+    def test_add_invalid_name_field(self):
+        body = {
+            "person_name": 1,
+            "contact_name": "TEST@TSET.COM",
+            "contact_gst_number": "123467213123121231231231"
+        }
+        response = self.client.post('/contacts', body, format='json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_invalid_phone_field(self):
+        body = {
+            "person_name": "TEST",
+            "contact_name": "TEST@TSET.COM",
+            "contact_gst_number": "123467213123121231231231"
+        }
+        response = self.client.post('/contacts', body, format='json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_invalid_email_field(self):
+        body = {
+            "person_name": "TEST",
+            "contact_name": "INVALIDEMAILVALUE",
+            "contact_gst_number": "123467213123121231231231"
+        }
+        response = self.client.post('/contacts', body, format='json')
+        self.assertEqual(response.status_code, 400)
+
+    # CRUD tests
     def test_add_contact(self):
         body = {
             "person_name": "TEST",
             "contact_name": "TEST@TSET.COM",
-            "contact_gst_number": "12346789"
+            "contact_gst_number": "12346721"
         }
         response = self.client.post('/contacts', body, format='json')
-        assert response.status_code == 201
+        self.assertEqual(response.status_code, 201)
 
     def test_get_contacts(self):
         for _ in range(10):
@@ -35,18 +64,24 @@ class ContactTests(TestCase):
                 contact_gst_number=f'123465-{_ + 1}'
             )
             contact.save()
-        response = self.client.get(f'/contacts?id={5}')
+        response = self.client.get(
+            f'/contacts?id={5}'
+        )
         self.assertEqual(response.data[0]['id'], 5)
 
     def test_update_contact(self):
         contact = Contact(
             person_name='test',
             contact_name='test@test.com',
-            contact_gst_number=f'1234656'
+            contact_gst_number='1234656'
         )
         contact.save()
-        response = self.client.put('/contacts', data={"id": contact.id, "person_name": "test-done"},
-                                   content_type='application/json')
+        response = self.client.put(
+            '/contacts',
+            data={"id": contact.id, "person_name": "test-done"},
+            content_type='application/json'
+        )
+        print(response.data)
         self.assertEqual(response.data['person_name'], 'test-done')
 
     def test_del_contact(self):
@@ -57,6 +92,8 @@ class ContactTests(TestCase):
                 contact_gst_number=f'123465-{_ + 1}'
             )
             contact.save()
-        response = self.client.delete('/contacts', data={"id": contact.id},
-                                      content_type='application/json')
+        response = self.client.delete(
+            '/contacts',
+            data={"id": contact.id},
+            content_type='application/json')
         self.assertEqual(len(response.data), 4)
